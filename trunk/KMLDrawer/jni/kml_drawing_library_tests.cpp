50c9a32f25daaa10cb0addadec988e0a950dda1f
joelodom@gmail.com <joelodom@gmail.com@210bedf8-9550-11dd-a7f8-5bf76e998c60>
2013-03-20 10:56:47 +0000

    Working on incorporating all dependencies for build.

---
+++ trunk/KMLDrawer/jni/kml_drawing_library_tests.cpp
@@ -0,0 +1,333 @@
+// Copyright (c) 1994-2013 Georgia Tech Research Corporation, Atlanta, GA
+// This file is part of FalconView(R).
+
+// FalconView(R) is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// FalconView(R) is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with FalconView(R).  If not, see <http://www.gnu.org/licenses/>.
+
+// FalconView(R) is a registered trademark of Georgia Tech Research Corporation.
+
+#include "kml_tests.h"
+#include "kml/convenience/convenience.h"
+#include <cmath>
+
+int main(int argc, char **argv) {
+   ::testing::InitGoogleTest(&argc, argv);
+   // gtest will delete new environment
+   ::testing::AddGlobalTestEnvironment(new KMLTestEnvironment);
+   return RUN_ALL_TESTS();
+}
+
+TEST(kml_drawing_library_tests, test_gtest)
+{
+   EXPECT_TRUE(true) << "law of contradiction failed";
+}
+
+TEST(kml_drawing_library_tests, test_fetch_kml_and_push_state)
+{
+   TestShim shim;
+   kmldrawing::KMLDrawer drawer(&shim);
+   ASSERT_TRUE(drawer.OpenKML(TEST_KML_NET_URL));
+}
+
+TEST(kml_drawing_library_tests, test_fetch_kml)
+{
+   // A KmlFile created by KmlCache retains a pointer back to this KmlCache,
+   // which is used by the style resolution machinery to fetch any styleUrls
+   // encountered.  A KmlFile created directly from KmlFile will not have a
+   // KmlCache to use and any remote style or schema references will be quietly
+   // ignored.
+
+   kmldrawing::KMLNetFetcher net_fetcher;
+   kmlengine::KmlCache kml_cache(&net_fetcher, 1234 /*max_size*/);
+
+   // show that the KML cache can fetch KML from an absolute URL
+
+   kmlengine::KmlFilePtr kml_file
+      = kml_cache.FetchKmlAbsolute(TEST_KML_NET_URL);
+   ASSERT_TRUE(kml_file != nullptr);
+
+   std::string serialized1;
+   kml_file->SerializeToString(&serialized1);
+
+   // show that the KML cache can fetch KML from a relative URL
+
+   kml_file
+      = kml_cache.FetchKmlRelative(TEST_KML_NET_BASE, TEST_KML_NET_TARGET);
+   ASSERT_TRUE(kml_file != nullptr);
+
+   std::string serialized2;
+   kml_file->SerializeToString(&serialized2);
+
+   EXPECT_STREQ(serialized1.c_str(), serialized2.c_str());
+
+   // show that the KML cache can fetch arbitrary data
+   std::string arbitrary_data;
+   ASSERT_TRUE(kml_cache.FetchDataRelative(
+      TEST_ARBITRARY_DATA_BASE, TEST_ARBITRARY_DATA_TARGET, &arbitrary_data));
+   EXPECT_STREQ(ARBITRARY_DATA.c_str(), arbitrary_data.c_str());
+}
+
+NON_UNIT_TEST(kml_drawing_library_tests, test_set_timer)
+{
+   TestShim shim;
+   kmldrawing::KMLDrawer drawer(&shim);
+   ASSERT_TRUE(drawer.OpenKML(TEST_REFRESH_ON_INTERVAL_KML));
+   EXPECT_EQ((size_t)0, shim.m_set_timer_calls);
+   drawer.Draw();
+   EXPECT_EQ((size_t)1, shim.m_set_timer_calls);
+}
+
+class TestCacheBehaviorNetFetcher : public kmlbase::NetFetcher
+{
+public:
+   bool m_should_fail;
+   TestCacheBehaviorNetFetcher() : m_should_fail(true) {}
+
+private:
+   virtual bool FetchUrl(const std::string& url, std::string* data) const
+   {
+      if (m_should_fail)
+         return false;
+      *data = TEST_COORDINATES_KML_DATA;
+      return true;
+   }
+};
+
+TEST(kml_drawing_library_tests, test_cache_behavior)
+{
+   // The drawing library assumes that if a fetch fails, the KmlCache
+   // will retry the URL if it is asked to fetch it again.  This test
+   // validates that assumption.
+
+   TestCacheBehaviorNetFetcher net_fetcher;
+   kmlengine::KmlCache kml_cache(&net_fetcher, 10);
+
+   // first we fail
+   kmlengine::KmlFilePtr kml_file = kml_cache.FetchKmlAbsolute(
+      TEST_KML_NET_URL);
+   EXPECT_TRUE(kml_file == nullptr);
+
+   // now we fetch
+   net_fetcher.m_should_fail = false;
+   kml_file = kml_cache.FetchKmlAbsolute(TEST_KML_NET_URL);
+   ASSERT_TRUE(kml_file != nullptr);
+   kmldom::ElementPtr root = kml_file->get_root();
+   EXPECT_TRUE(root != nullptr);
+}
+
+TEST(kml_drawing_library_tests, test_parse_kml_datetime)
+{
+   std::string date_time, error;
+   struct tm tm_val;
+   int offset_hours, offset_minutes;
+   bool negative_offset;
+
+   date_time = "1997";
+   EXPECT_TRUE(kmldrawing::ParseKMLDateTime(date_time, &tm_val, &offset_hours,
+      &offset_minutes, &negative_offset, &error));
+   EXPECT_EQ((size_t)0, error.length());
+   EXPECT_EQ(97, tm_val.tm_year);
+   EXPECT_EQ(0, tm_val.tm_mon);
+   EXPECT_EQ(1, tm_val.tm_mday);
+   EXPECT_EQ(0, tm_val.tm_hour);
+   EXPECT_EQ(0, tm_val.tm_min);
+   EXPECT_EQ(0, tm_val.tm_sec);
+   EXPECT_EQ(0, offset_hours);
+   EXPECT_EQ(0, offset_minutes);
+   EXPECT_FALSE(negative_offset);
+
+   date_time = "1997-07";
+   EXPECT_TRUE(kmldrawing::ParseKMLDateTime(date_time, &tm_val, &offset_hours,
+      &offset_minutes, &negative_offset, &error));
+   EXPECT_EQ((size_t)0, error.length());
+   EXPECT_EQ(97, tm_val.tm_year);
+   EXPECT_EQ(6, tm_val.tm_mon);
+   EXPECT_EQ(1, tm_val.tm_mday);
+   EXPECT_EQ(0, tm_val.tm_hour);
+   EXPECT_EQ(0, tm_val.tm_min);
+   EXPECT_EQ(0, tm_val.tm_sec);
+   EXPECT_EQ(0, offset_hours);
+   EXPECT_EQ(0, offset_minutes);
+   EXPECT_FALSE(negative_offset);
+
+   date_time = "1997-07-16";
+   EXPECT_TRUE(kmldrawing::ParseKMLDateTime(date_time, &tm_val, &offset_hours,
+      &offset_minutes, &negative_offset, &error));
+   EXPECT_EQ((size_t)0, error.length());
+   EXPECT_EQ(97, tm_val.tm_year);
+   EXPECT_EQ(6, tm_val.tm_mon);
+   EXPECT_EQ(16, tm_val.tm_mday);
+   EXPECT_EQ(0, tm_val.tm_hour);
+   EXPECT_EQ(0, tm_val.tm_min);
+   EXPECT_EQ(0, tm_val.tm_sec);
+   EXPECT_EQ(0, offset_hours);
+   EXPECT_EQ(0, offset_minutes);
+   EXPECT_FALSE(negative_offset);
+
+   date_time = "1997-07-16T07:30:15Z";
+   EXPECT_TRUE(kmldrawing::ParseKMLDateTime(date_time, &tm_val, &offset_hours,
+      &offset_minutes, &negative_offset, &error));
+   EXPECT_EQ((size_t)0, error.length());
+   EXPECT_EQ(97, tm_val.tm_year);
+   EXPECT_EQ(6, tm_val.tm_mon);
+   EXPECT_EQ(16, tm_val.tm_mday);
+   EXPECT_EQ(7, tm_val.tm_hour);
+   EXPECT_EQ(30, tm_val.tm_min);
+   EXPECT_EQ(15, tm_val.tm_sec);
+   EXPECT_EQ(0, offset_hours);
+   EXPECT_EQ(0, offset_minutes);
+   EXPECT_FALSE(negative_offset);
+
+   date_time = "1997-07-16T10:30:15+03:00";
+   EXPECT_TRUE(kmldrawing::ParseKMLDateTime(date_time, &tm_val, &offset_hours,
+      &offset_minutes, &negative_offset, &error));
+   EXPECT_EQ((size_t)0, error.length());
+   EXPECT_EQ(97, tm_val.tm_year);
+   EXPECT_EQ(6, tm_val.tm_mon);
+   EXPECT_EQ(16, tm_val.tm_mday);
+   EXPECT_EQ(10, tm_val.tm_hour);
+   EXPECT_EQ(30, tm_val.tm_min);
+   EXPECT_EQ(15, tm_val.tm_sec);
+   EXPECT_EQ(3, offset_hours);
+   EXPECT_EQ(0, offset_minutes);
+   EXPECT_FALSE(negative_offset);
+}
+
+TEST(kml_drawing_library_tests, test_feature_and_ancestors_visible)
+{
+   kmldom::KmlFactory* factory = kmldom::KmlFactory::GetFactory();
+   kmldom::FolderPtr folder = factory->CreateFolder();
+   kmldom::PlacemarkPtr placemark = factory->CreatePlacemark();
+   folder->add_feature(placemark);
+
+   EXPECT_TRUE(kmldrawing::FeatureAndAncestorsVisible(folder));
+   EXPECT_TRUE(kmldrawing::FeatureAndAncestorsVisible(placemark));
+
+   folder->set_visibility(false);
+
+   EXPECT_FALSE(kmldrawing::FeatureAndAncestorsVisible(folder));
+   EXPECT_FALSE(kmldrawing::FeatureAndAncestorsVisible(placemark));
+}
+
+NON_UNIT_TEST(kml_drawing_library_tests, test_kmz_internal_space)
+{
+   // the KMZ file fetched below has an internal file with a space in it
+   TestShim shim;
+   kmldrawing::KMLDrawer drawer(&shim);
+   ASSERT_TRUE(drawer.OpenKML(
+      "http://osm-kml.appspot.com/static/internal_file_has_space.kmz"));
+   EXPECT_EQ((size_t)0, shim.m_draw_image_calls);
+   drawer.Draw();
+   EXPECT_EQ((size_t)1, shim.m_draw_image_calls);
+}
+
+TEST(kml_drawing_library_tests, test_get_bbox_from_geometry)
+{
+   // create a KML file with a multi geometry
+   std::string errors;
+   kmlengine::KmlFilePtr kml_file = kmlengine::KmlFile::CreateFromParse(
+      std::string(TEST_MULTI_GEOMETRY_KML_DATA), &errors);
+   EXPECT_EQ((size_t)0, errors.length());
+   ASSERT_TRUE(kml_file != nullptr);
+
+   // drill down to the multi geometry
+   kmldom::KmlPtr kml = kmldom::AsKml(kml_file->get_root());
+   ASSERT_TRUE(kml != nullptr);
+   kmldom::PlacemarkPtr placemark = kmldom::AsPlacemark(kml->get_feature());
+   ASSERT_TRUE(placemark != nullptr);
+   EXPECT_TRUE(placemark->has_geometry());
+
+   // test GetBboxFromGeometry
+   kmlengine::Bbox bbox;
+   EXPECT_TRUE(
+      kmldrawing::GetBboxFromGeometry(placemark->get_geometry(), &bbox));
+   EXPECT_EQ(2.0, bbox.get_north());
+   EXPECT_EQ(0.0, bbox.get_south());
+   EXPECT_EQ(6.0, bbox.get_east());
+   EXPECT_EQ(0.0, bbox.get_west());
+}
+
+TEST(kml_drawing_library_tests, test_quad_tile_random_points)
+{
+   // This is a rather inefficient probabilistic test, but it should
+   // statistically excercise many of the decision branches in the
+   // quad tile scheme.
+
+   int NUM_HIT_TESTS = 1000;
+   const int NUM_POINTS = 1000;
+   const double LAT_MAX_MIN = 5.0;
+   const double LON_MAX_MIN = 5.0;
+
+   // generate random placemarks
+
+   srand((unsigned int)time(nullptr));
+   kmldrawing::QuadTile top_tile;
+
+   kmldom::PlacemarkPtr placemarks[NUM_POINTS];
+
+   for (size_t i = 0; i < NUM_POINTS; i++)
+   {
+      std::stringstream ss;
+      ss << i;
+
+      double lat = 2*LAT_MAX_MIN*rand()/RAND_MAX - LAT_MAX_MIN;
+      double lon = 2*LON_MAX_MIN*rand()/RAND_MAX - LON_MAX_MIN;
+
+      placemarks[i] = kmlconvenience::CreatePointPlacemark(ss.str(), lat, lon);
+
+      top_tile.AddObject(placemarks[i]);
+   }
+
+   // hit test random points
+
+   while (NUM_HIT_TESTS-- > 0)
+   {
+      double lat = 2*LAT_MAX_MIN*rand()/RAND_MAX - LAT_MAX_MIN;
+      double lon = 2*LON_MAX_MIN*rand()/RAND_MAX - LON_MAX_MIN;
+
+      kmldom::ObjectPtr obj;
+      top_tile.FindNearest(lat, lon, &obj);
+
+      kmldom::PlacemarkPtr placemark = kmldom::AsPlacemark(obj);
+      ASSERT_TRUE(placemark != nullptr);
+
+      // check it the slow way
+
+      kmldom::PlacemarkPtr nearest;
+      double nearest_distance = DBL_MAX;
+
+      for (size_t i = 0; i < NUM_POINTS; i++)
+      {
+         const kmldom::PointPtr point
+            = kmldom::AsPoint(placemarks[i]->get_geometry());
+         const kmlbase::Vec3 vec3
+            = point->get_coordinates()->get_coordinates_array_at(0);
+
+         double dx2 = vec3.get_longitude() - lon;
+         dx2 *= dx2;
+
+         double dy2 = vec3.get_latitude() - lat;
+         dy2 *= dy2;
+
+         double d = ::sqrt(dx2 + dy2); // use real sqrt for testing
+
+         if (d < nearest_distance)
+         {
+            nearest = placemarks[i];
+            nearest_distance = d;
+         }
+      }
+
+      EXPECT_STREQ(nearest->get_name().c_str(), placemark->get_name().c_str());
+   }
+}
---
+++ trunk/KMLDrawer/jni/kml_tests.h
@@ -0,0 +1,413 @@
+// Copyright (c) 1994-2013 Georgia Tech Research Corporation, Atlanta, GA
+// This file is part of FalconView(R).
+
+// FalconView(R) is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// FalconView(R) is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with FalconView(R).  If not, see <http://www.gnu.org/licenses/>.
+
+// FalconView(R) is a registered trademark of Georgia Tech Research Corporation.
+
+#ifndef KML_TESTS_H_
+#define KML_TESTS_H_
+
+//#define ENABLE_NON_UNIT_TESTS
+
+#ifdef ENABLE_NON_UNIT_TESTS
+#pragma message("WARNING: Disable non-unit tests before commiting to SVN.")
+#define NON_UNIT_TEST(test_case_name, test_name) TEST(test_case_name, test_name)
+#else
+#define NON_UNIT_TEST(test_case_name, test_name) \
+   TEST(test_case_name, DISABLED_##test_name)
+#endif
+
+#include "gtest/gtest.h"
+#include "kml/dom/xsd.h"
+#include "kml/engine.h"
+#include "KMLDrawingLibrary.h"
+
+extern std::string TEST_KML;
+extern std::string STYLES_KML;
+extern std::string ARBITRARY_DATA;
+
+extern const char* TEST_KML_NET_URL;
+extern const char* TEST_KML_NET_BASE;
+extern const char* TEST_KML_NET_TARGET;
+
+extern const char* TEST_STYLES_KML_URL;
+
+extern const char* TEST_ARBITRARY_DATA_URL;
+extern const char* TEST_ARBITRARY_DATA_BASE;
+extern const char* TEST_ARBITRARY_DATA_TARGET;
+
+extern const char* TEST_VIEWFORMAT_URL;
+extern std::string TEST_VIEWFORMAT_KML;
+
+extern const char* TEST_HTTPQUERY_URL;
+extern std::string TEST_HTTPQUERY1_KML_DATA;
+extern std::string TEST_HTTPQUERY2_KML_DATA;
+extern std::string TEST_HTTPQUERY3_KML_DATA;
+
+extern std::string TEST_TREE_CONTROL_KML_DATA;
+
+extern const char* SETTABLE_KML_URL;
+extern std::string g_settable_kml;
+
+//
+// the following data may be saved with the save_all_test_kml "test"
+//
+
+static const char* TEST_COORDINATES_KML
+   = "D:\\exclude\\temp\\coordinates.kml";
+extern std::string TEST_COORDINATES_KML_DATA;
+
+static const char* TEST_OUTER_BOUNDARY_KML
+   = "D:\\exclude\\temp\\outer_boundary.kml";
+extern std::string TEST_OUTER_BOUNDARY_KML_DATA;
+
+static const char* TEST_LINE_STRING_KML
+   = "D:\\exclude\\temp\\line_string.kml";
+extern std::string TEST_LINE_STRING_KML_DATA;
+
+static const char* TEST_MULTI_GEOMETRY_KML
+   = "D:\\exclude\\temp\\multi_geometry.kml";
+extern std::string TEST_MULTI_GEOMETRY_KML_DATA;
+
+static const char* TEST_ICON_STYLE_KML
+   = "D:\\exclude\\temp\\icon_style.kml";
+extern std::string TEST_ICON_STYLE_KML_DATA;
+
+static const char* TEST_ICON_COLOR_KML
+   = "D:\\exclude\\temp\\icon_color.kml";
+extern std::string TEST_ICON_COLOR_KML_DATA;
+
+static const char* TEST_POLY_STYLE_KML
+   = "D:\\exclude\\temp\\poly_style.kml";
+extern std::string TEST_POLY_STYLE_KML_DATA;
+
+static const char* TEST_LINE_STYLE_KML
+   = "D:\\exclude\\temp\\line_style.kml";
+extern std::string TEST_LINE_STYLE_KML_DATA;
+
+static const char* TEST_GROUND_OVERLAY_KML
+   = "D:\\exclude\\temp\\ground_overlay.kml";
+extern std::string TEST_GROUND_OVERLAY_KML_DATA;
+
+static const char* TEST_SUPER_OVERLAY_KML
+   = "D:\\exclude\\temp\\super_overlay.kml";
+extern std::string TEST_SUPER_OVERLAY_KML_DATA;
+
+static const char* TEST_NETWORK_LINK_KML
+   = "D:\\exclude\\temp\\network_link.kml";
+extern std::string TEST_NETWORK_LINK_KML_DATA;
+
+static const char* TEST_RELATIVE_LOAD_KML
+   = "D:\\exclude\\temp\\relative_load.kml";
+extern std::string TEST_RELATIVE_LOAD_KML_DATA;
+
+static const char* TEST_NETWORK_SHARED_STYLES_KML
+   = "D:\\exclude\\temp\\network_shared_styles.kml";
+extern std::string TEST_NETWORK_SHARED_STYLES_KML_DATA;
+
+static const char* TEST_KMZ_WITHOUT_EXTENSION_KML
+   = "D:\\exclude\\temp\\kmz_without_extension.kml";
+extern std::string TEST_KMZ_WITHOUT_EXTENSION_KML_DATA;
+
+static const char* TEST_SCREEN_OVERLAY_OVERLAY_XY_KML
+   = "D:\\exclude\\temp\\screen_overlay_overlay_xy.kml";
+extern std::string TEST_SCREEN_OVERLAY_OVERLAY_XY_KML_DATA;
+
+static const char* TEST_SCREEN_OVERLAY_SCREEN_XY_KML
+   = "D:\\exclude\\temp\\screen_overlay_screen_xy.kml";
+extern std::string TEST_SCREEN_OVERLAY_SCREEN_XY_KML_DATA;
+
+static const char* TEST_SCREEN_OVERLAY_ROTATION_KML
+   = "D:\\exclude\\temp\\screen_overlay_rotation.kml";
+extern std::string TEST_SCREEN_OVERLAY_ROTATION_KML_DATA;
+
+static const char* TEST_SCREEN_OVERLAY_SIZE_KML
+   = "D:\\exclude\\temp\\screen_overlay_size.kml";
+extern std::string TEST_SCREEN_OVERLAY_SIZE_KML_DATA;
+
+static const char* TEST_NETWORK_LINK_WINFORMAT_KML
+   = "D:\\exclude\\temp\\network_link_winformat.kml";
+extern std::string TEST_NETWORK_LINK_WINFORMAT_KML_DATA;
+
+static const char* TEST_NETWORK_LINK_LOCAL_RELATIVE_KML
+   = "D:\\exclude\\temp\\network_link_local_relative.kml";
+extern std::string TEST_NETWORK_LINK_LOCAL_RELATIVE_KML_DATA;
+
+static const char* TEST_KML_REDIRECT_KML
+   = "D:\\exclude\\temp\\kml_redirect.kml";
+extern std::string TEST_KML_REDIRECT_KML_DATA;
+
+static const char* TEST_RELATIVE_KMZ_NESTED_KML
+   = "D:\\exclude\\temp\\relative_kmz_nested.kml";
+extern std::string TEST_RELATIVE_KMZ_NESTED_KML_DATA;
+
+static const char* TEST_STYLE_MAP_KML
+   = "D:\\exclude\\temp\\style_map.kml";
+extern std::string TEST_STYLE_MAP_KML_DATA;
+
+static const char* TEST_BALLOON_STYLE_KML
+   = "D:\\exclude\\temp\\balloon_style.kml";
+extern std::string TEST_BALLOON_STYLE_KML_DATA;
+
+static const char* TEST_POLY_FILLS_KML
+   = "D:\\exclude\\temp\\poly_fills.kml";
+extern std::string TEST_POLY_FILLS_KML_DATA;
+
+static const char* TEST_LABEL_STYLE_KML
+   = "D:\\exclude\\temp\\label_style.kml";
+extern std::string TEST_LABEL_STYLE_KML_DATA;
+
+static const char* TEST_REFRESH_ON_INTERVAL_KML
+   = "D:\\exclude\\temp\\refresh_on_interval.kml";
+extern std::string TEST_REFRESH_ON_INTERVAL_KML_DATA;
+
+static const char* TEST_POLY_INNER_RINGS_KML
+   = "D:\\exclude\\temp\\poly_inner_rings.kml";
+extern std::string TEST_POLY_INNER_RINGS_KML_DATA;
+
+static const char* TEST_POLY_INNER_RINGS_NOFILL_KML
+   = "D:\\exclude\\temp\\poly_inner_rings_nofill.kml";
+extern std::string TEST_POLY_INNER_RINGS_NOFILL_KML_DATA;
+
+static const char* TEST_TIME_KML
+   = "D:\\exclude\\temp\\time.kml";
+extern std::string TEST_TIME_KML_DATA;
+
+static const char* TEST_PHOTO_OVERLAY_KML
+   = "D:\\exclude\\temp\\photo_overlay.kml";
+extern std::string TEST_PHOTO_OVERLAY_KML_DATA;
+
+static const char* TEST_STYLE_WORK_AROUND_KML
+   = "D:\\exclude\\temp\\style_work_around.kml";
+extern std::string TEST_STYLE_WORK_AROUND_KML_DATA;
+
+static const char* TEST_HIT_TEST_INVISIBLE_FOLDER_KML
+   = "D:\\exclude\\temp\\hit_test_invisible_folder.kml";
+extern std::string TEST_HIT_TEST_INVISIBLE_FOLDER_KML_DATA;
+
+static const char* ONLY_TIMESTAMPS_KML
+   = "D:\\exclude\\temp\\only_timestamps.kml";
+extern std::string ONLY_TIMESTAMPS_KML_DATA;
+
+static const char* TEST_SCREEN_OVERLAY_WIN_PATH_KML
+   = "D:\\exclude\\temp\\win_path.kml";
+extern std::string TEST_SCREEN_OVERLAY_WIN_PATH_KML_DATA;
+
+static const char* TEST_EXPLICIT_STYLE_URL_KML
+   = "D:\\exclude\\temp\\explicit_style_url.kml";
+extern std::string TEST_EXPLICIT_STYLE_URL_KML_DATA;
+
+static const char* TEST_MODEL_KML = "D:\\exclude\\temp\\model.kml";
+extern std::string TEST_MODEL_KML_DATA;
+
+//
+// end data that may be saved with the save_all_test_kml "test"
+//
+
+extern bool FetchAsTestDataImpl(const std::string& url, std::string* data);
+
+class KMLTestEnvironment : public ::testing::Environment
+{
+public:
+   virtual ~KMLTestEnvironment() {}
+
+   virtual void SetUp()
+   {
+      kmldrawing::FetchAsTestData = FetchAsTestDataImpl;
+      kmldrawing::InTestEnvironment = true;
+
+      m_factory = kmldom::KmlFactory::GetFactory();
+      m_xsd = kmldom::Xsd::GetSchema();
+
+      // parse the test KML once here
+      TEST_KML_ROOT = kmldom::Parse(TEST_KML, NULL);
+      ASSERT_TRUE(TEST_KML_ROOT != nullptr);
+
+      // build a map of test file names to test data
+
+      kmldrawing::TestDataMap[TEST_COORDINATES_KML] = TEST_COORDINATES_KML_DATA;
+      kmldrawing::TestDataMap[TEST_OUTER_BOUNDARY_KML]
+         = TEST_OUTER_BOUNDARY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_LINE_STRING_KML] = TEST_LINE_STRING_KML_DATA;
+      kmldrawing::TestDataMap[TEST_MULTI_GEOMETRY_KML]
+         = TEST_MULTI_GEOMETRY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_ICON_STYLE_KML] = TEST_ICON_STYLE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_ICON_COLOR_KML] = TEST_ICON_COLOR_KML_DATA;
+      kmldrawing::TestDataMap[TEST_POLY_STYLE_KML] = TEST_POLY_STYLE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_LINE_STYLE_KML] = TEST_LINE_STYLE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_GROUND_OVERLAY_KML]
+         = TEST_GROUND_OVERLAY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_SUPER_OVERLAY_KML]
+         = TEST_SUPER_OVERLAY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_NETWORK_LINK_KML]
+         = TEST_NETWORK_LINK_KML_DATA;
+      kmldrawing::TestDataMap[TEST_RELATIVE_LOAD_KML]
+         = TEST_RELATIVE_LOAD_KML_DATA;
+      kmldrawing::TestDataMap[TEST_NETWORK_SHARED_STYLES_KML]
+         = TEST_NETWORK_SHARED_STYLES_KML_DATA;
+      kmldrawing::TestDataMap[TEST_KMZ_WITHOUT_EXTENSION_KML]
+         = TEST_KMZ_WITHOUT_EXTENSION_KML_DATA;
+      kmldrawing::TestDataMap[TEST_SCREEN_OVERLAY_OVERLAY_XY_KML]
+         = TEST_SCREEN_OVERLAY_OVERLAY_XY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_SCREEN_OVERLAY_SCREEN_XY_KML]
+         = TEST_SCREEN_OVERLAY_SCREEN_XY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_SCREEN_OVERLAY_ROTATION_KML]
+         = TEST_SCREEN_OVERLAY_ROTATION_KML_DATA;
+      kmldrawing::TestDataMap[TEST_SCREEN_OVERLAY_SIZE_KML]
+         = TEST_SCREEN_OVERLAY_SIZE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_NETWORK_LINK_WINFORMAT_KML]
+         = TEST_NETWORK_LINK_WINFORMAT_KML_DATA;
+      kmldrawing::TestDataMap[TEST_NETWORK_LINK_LOCAL_RELATIVE_KML]
+         = TEST_NETWORK_LINK_LOCAL_RELATIVE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_KML_REDIRECT_KML]
+         = TEST_KML_REDIRECT_KML_DATA;
+      kmldrawing::TestDataMap[TEST_RELATIVE_KMZ_NESTED_KML]
+         = TEST_RELATIVE_KMZ_NESTED_KML_DATA;
+      kmldrawing::TestDataMap[TEST_STYLE_MAP_KML] = TEST_STYLE_MAP_KML_DATA;
+      kmldrawing::TestDataMap[TEST_BALLOON_STYLE_KML]
+         = TEST_BALLOON_STYLE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_POLY_FILLS_KML] = TEST_POLY_FILLS_KML_DATA;
+      kmldrawing::TestDataMap[TEST_LABEL_STYLE_KML] = TEST_LABEL_STYLE_KML_DATA;
+      kmldrawing::TestDataMap[TEST_REFRESH_ON_INTERVAL_KML]
+         = TEST_REFRESH_ON_INTERVAL_KML_DATA;
+      kmldrawing::TestDataMap[TEST_POLY_INNER_RINGS_KML]
+         = TEST_POLY_INNER_RINGS_KML_DATA;
+      kmldrawing::TestDataMap[TEST_POLY_INNER_RINGS_NOFILL_KML]
+         = TEST_POLY_INNER_RINGS_NOFILL_KML_DATA;
+      kmldrawing::TestDataMap[TEST_TIME_KML] = TEST_TIME_KML_DATA;
+      kmldrawing::TestDataMap[TEST_PHOTO_OVERLAY_KML]
+         = TEST_PHOTO_OVERLAY_KML_DATA;
+      kmldrawing::TestDataMap[TEST_STYLE_WORK_AROUND_KML]
+         = TEST_STYLE_WORK_AROUND_KML_DATA;
+      kmldrawing::TestDataMap[TEST_HIT_TEST_INVISIBLE_FOLDER_KML]
+         = TEST_HIT_TEST_INVISIBLE_FOLDER_KML_DATA;
+      kmldrawing::TestDataMap[ONLY_TIMESTAMPS_KML] = ONLY_TIMESTAMPS_KML_DATA;
+      kmldrawing::TestDataMap[TEST_SCREEN_OVERLAY_WIN_PATH_KML]
+         = TEST_SCREEN_OVERLAY_WIN_PATH_KML_DATA;
+      kmldrawing::TestDataMap[TEST_EXPLICIT_STYLE_URL_KML]
+         = TEST_EXPLICIT_STYLE_URL_KML_DATA;
+      kmldrawing::TestDataMap[TEST_MODEL_KML] = TEST_MODEL_KML_DATA;
+   }
+
+   virtual void TearDown()
+   {
+      // We use this testing environment to delete singleton objects in testing.
+      // In a typical application, we would not do this.  We allow it here
+      // since this will only happen after all of the tests are completed.  This
+      // helps us to detect leaks.
+
+      delete m_factory;
+      delete m_xsd;
+   }
+
+   static bool verify_url_fetch_count()
+   {
+      // report a failure if we fetched the same URL more than once
+      for (auto it = kmldrawing::URLFetchCount.begin();
+         it != kmldrawing::URLFetchCount.end(); it++)
+      {
+         if (it->second != 1)
+            return false;
+      }
+      return true;
+   }
+
+   static kmldom::ElementPtr TEST_KML_ROOT;
+
+   static size_t s_viewformat_url_fetch_count;
+   static size_t s_httpquery_url_fetch_count;
+
+private:
+   kmldom::KmlFactory* m_factory;
+   kmldom::Xsd* m_xsd;
+};
+
+// A feature visitor can walk an entire kml hierarchy, or a subset of
+// a KML hierarchy.
+class TestFeatureVisitor : public kmlengine::FeatureVisitor
+{
+public:
+   TestFeatureVisitor() : features_visited(0) {}
+
+   virtual void VisitFeature(const kmldom::FeaturePtr& feature)
+   {
+      // ... per-feature code ...
+      ++features_visited;
+   }
+
+   int features_visited;
+};
+
+class TestShimImage : public kmldrawing::Image
+{
+   virtual void GetSize(int* width, int* height) const
+   {
+      *width = 256;
+      *height = 256;
+   }
+};
+
+class TestShim : public kmldrawing::KMLShim
+{
+public:
+   size_t m_set_timer_calls;
+   size_t m_draw_image_calls;
+
+   TestShim() : m_set_timer_calls(0), m_draw_image_calls(0) {}
+
+   virtual void BeginDraw();
+   virtual void EndDraw();
+   virtual void BeginDrawElement(const kmldom::ElementPtr& element);
+   virtual void EndDrawElement(const kmldom::ElementPtr& element);
+   virtual void GetSurfaceSize(int* width, int* height);
+   virtual void GetMapBounds(double* bottom_lat, double* left_lon,
+      double* top_lat, double* right_lon);
+   virtual void GetDegreesPerPixel(double* dpp_y, double* dpp_x);
+   virtual void GeoToSurface(double lat, double lon, int* x, int* y);
+   virtual void SurfaceToGeo(int x, int y, double* lat, double* lon);
+   virtual void CreatePen(
+      const kmlbase::Color32& color, float width, kmldrawing::Pen** pen);
+   virtual void CreateBrush(
+      const kmlbase::Color32& color, kmldrawing::Brush** brush);
+   virtual void CreateImageFromRawBytes(
+      unsigned char* bytes, size_t length, kmldrawing::Image** image,
+      bool will_be_used_for_geo = false);
+   virtual void DrawImage(const kmldrawing::Image& image,
+      float left, float top, float right, float bottom, float rotation);
+   virtual void DrawImageGeo(const kmldrawing::Image& image,
+      double west, double north, double east, double south);
+   virtual void DrawEllipse(float x, float y, float x_axis, float y_axis,
+      const kmldrawing::Pen& pen, const kmldrawing::Brush& brush);
+   virtual void DrawPolyPolygon(
+      const std::vector<std::pair<float, float>>& points,
+      const std::vector<int>& point_counts,
+      const kmldrawing::Pen& pen, const kmldrawing::Brush& brush);
+   virtual void DrawLines(const std::vector<std::pair<float, float>>& points,
+      const kmldrawing::Pen& pen);
+   virtual void DrawString(const std::string& text, float x, float y,
+      float size, const kmldrawing::Pen& pen);
+   virtual void ShowInformation(
+      const std::string& title, const std::string& text);
+   virtual void SetTimer(int id, unsigned long ms);
+   virtual void ClearAllTimers();
+   virtual void Invalidate(void* ptr);
+   virtual bool Fetch(const std::string& uri, std::string* data);
+   virtual bool ShouldRender(const kmldom::TimePrimitivePtr& time_primitive);
+
+private:
+   TestShimImage m_image;
+};
+
+#endif // #ifndef KML_TESTS_H_
