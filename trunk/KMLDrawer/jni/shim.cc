3b5fd3c0459b6e4f9716e0bf0cffb3fc4c3dd96d
joelodom@gmail.com <joelodom@gmail.com@210bedf8-9550-11dd-a7f8-5bf76e998c60>
2013-04-15 16:57:08 +0000

    KML network fetching is now working.

--- trunk/KMLDrawer/jni/shim.cc
+++ trunk/KMLDrawer/jni/shim.cc
@@ -9,39 +9,39 @@
 
 extern "C"
 {
-	JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_nativeLog(
-			JNIEnv* env, jobject obj, jstring logThis);
-	JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_initDrawer(
-			JNIEnv* env, jobject obj, jstring kml_url);
-	JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_drawKML(
-			JNIEnv* env, jobject obj, jint height, jint width);
+JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_nativeLog(
+      JNIEnv* env, jobject obj, jstring logThis);
+JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_initDrawer(
+      JNIEnv* env, jobject obj, jstring kml_url);
+JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_drawKML(
+      JNIEnv* env, jobject obj, jint height, jint width);
 };
 
 extern int run_tests_main(int argc, char **argv);
 bool tests_run = false;
 
 JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_nativeLog(
-		JNIEnv* env, jobject obj, jstring logThis)
+      JNIEnv* env, jobject obj, jstring logThis)
 {
-	// log
-    jboolean isCopy;
-    const char * szLogThis = env->GetStringUTFChars(logThis, &isCopy);
-    __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "NDK:LC: [%s]", szLogThis);
-    env->ReleaseStringUTFChars(logThis, szLogThis);
+   // log
+   jboolean isCopy;
+   const char * szLogThis = env->GetStringUTFChars(logThis, &isCopy);
+   __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "NDK:LC: [%s]", szLogThis);
+   env->ReleaseStringUTFChars(logThis, szLogThis);
 
-    // run tests on first call
-    if (!tests_run)
-    {
-    	char* argv[1] = {(char*)"foo"};
-    	int argc = 1;
-    	int rv = run_tests_main(argc, argv);
-    	tests_run = true;
+   // run tests on first call
+   if (!tests_run)
+   {
+      char* argv[1] = {(char*)"foo"};
+      int argc = 1;
+      int rv = run_tests_main(argc, argv);
+      tests_run = true;
 
-    	std::stringstream ss;
-    	ss << "Tests returned: " << rv;
+      std::stringstream ss;
+      ss << "Tests returned: " << rv;
 
-    	__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, ss.str().c_str());
-    }
+      __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, ss.str().c_str());
+   }
 }
 
 ////
@@ -50,37 +50,46 @@ JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_nativeLog(
 
 AndroidShim* the_shim = nullptr;
 kmldrawing::KMLDrawer* the_drawer = nullptr;
+std::string url_to_open;
+bool opened = false;
 
 JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_initDrawer(
-		JNIEnv* env, jobject obj, jstring kml_url)
+      JNIEnv* env, jobject obj, jstring kml_url)
 {
-    __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "Initializing drawer...");
+   __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "Initializing drawer...");
 
-	// instantiate the objects
-	the_shim = new AndroidShim;
-	the_drawer = new kmldrawing::KMLDrawer(the_shim);
+   // initialize shim & drawer
+   the_shim = new AndroidShim;
+   the_drawer = new kmldrawing::KMLDrawer(the_shim);
 
-	// open the kml
-    jboolean is_copy;
-    const char* url = env->GetStringUTFChars(kml_url, &is_copy);
-	if (the_drawer->OpenKML(url))
-		__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "Open SUCCESS");
-	else
-		__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "Open FAILED");
-    env->ReleaseStringUTFChars(kml_url, url);
+   // calling back into fetchURL causes strange problems here,
+   // so we will do the open on the first draw
+   jboolean is_copy;
+   const char* url = env->GetStringUTFChars(kml_url, &is_copy);
+   url_to_open = url;
+   env->ReleaseStringUTFChars(kml_url, url);
 }
 
 JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_drawKML(
-		JNIEnv* env, jobject obj, jint height, jint width)
+      JNIEnv* env, jobject obj, jint height, jint width)
 {
-	the_shim->m_height = height;
-	the_shim->m_width = width;
-	the_shim->m_env = env;
-	the_shim->m_obj = obj;
+   the_shim->m_height = height;
+   the_shim->m_width = width;
+   the_shim->m_env = env;
+   the_shim->m_obj = obj;
 
-	the_drawer->Draw();
+   if (!opened)
+   {
+      if (the_drawer->OpenKML(url_to_open))
+         __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "OpenKML SUCCESS");
+      else
+         __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "OpenKML FAILED");
+      opened = true;
+   }
 
-	the_shim->CleanupAfterDraw();
+   the_drawer->Draw();
+
+   the_shim->CleanupAfterDraw();
 }
 
 ////
@@ -89,125 +98,125 @@ JNIEXPORT void JNICALL Java_org_falconview_kmldrawer_MainActivity_drawKML(
 
 void AndroidShim::BeginDraw()
 {
-	//LogMessage("NOT IMPLEMENTED: BeginDraw");
+   //LogMessage("NOT IMPLEMENTED: BeginDraw");
 }
 
 void AndroidShim::EndDraw()
 {
-	//LogMessage("NOT IMPLEMENTED: EndDraw");
+   //LogMessage("NOT IMPLEMENTED: EndDraw");
 }
 
 void AndroidShim::BeginDrawElement(const kmldom::ElementPtr& element)
 {
-	//LogMessage("NOT IMPLEMENTED: BeginDrawElement");
+   //LogMessage("NOT IMPLEMENTED: BeginDrawElement");
 }
 
 void AndroidShim::EndDrawElement(const kmldom::ElementPtr& element)
 {
-	//LogMessage("NOT IMPLEMENTED: EndDrawElement");
+   //LogMessage("NOT IMPLEMENTED: EndDrawElement");
 }
 
 void AndroidShim::GetSurfaceSize(int* width, int* height)
 {
-	*width = m_width;
-	*height = m_height;
+   *width = m_width;
+   *height = m_height;
 
-//	std::stringstream ss;
-//	ss << "GetSurfaceSize Height: " << m_height;
-//	LogMessage(ss.str().c_str());
+   //	std::stringstream ss;
+   //	ss << "GetSurfaceSize Height: " << m_height;
+   //	LogMessage(ss.str().c_str());
 }
 
 void AndroidShim::GetMapBounds(double* bottom_lat, double* left_lon,
       double* top_lat, double* right_lon)
 {
-	//LogMessage("NOT IMPLEMENTED: GetMapBounds");
-	*bottom_lat = 23.0;
-	*left_lon = -127.0;
-	*top_lat = 50.0;
-	*right_lon = -67.0;
+   //LogMessage("NOT IMPLEMENTED: GetMapBounds");
+   *bottom_lat = 23.0;
+   *left_lon = -127.0;
+   *top_lat = 50.0;
+   *right_lon = -67.0;
 }
 
 void AndroidShim::GetDegreesPerPixel(double* dpp_y, double* dpp_x)
 {
-	//LogMessage("NOT IMPLEMENTED: GetDegreesPerPixel");
-	double bottom_lat, left_lon, top_lat, right_lon;
-	GetMapBounds(&bottom_lat, &left_lon, &top_lat, &right_lon);
-	*dpp_y = (top_lat - bottom_lat) / m_height;
-	*dpp_x = (right_lon - left_lon) / m_width;
+   //LogMessage("NOT IMPLEMENTED: GetDegreesPerPixel");
+   double bottom_lat, left_lon, top_lat, right_lon;
+   GetMapBounds(&bottom_lat, &left_lon, &top_lat, &right_lon);
+   *dpp_y = (top_lat - bottom_lat) / m_height;
+   *dpp_x = (right_lon - left_lon) / m_width;
 }
 
 void AndroidShim::GeoToSurface(double lat, double lon, int* x, int* y)
 {
-	//LogMessage("NOT IMPLEMENTED: GeoToSurface");
-	double dpp_y, dpp_x;
-	double bottom_lat, left_lon, top_lat, right_lon;
-	GetDegreesPerPixel(&dpp_y, &dpp_x);
-	GetMapBounds(&bottom_lat, &left_lon, &top_lat, &right_lon);
-	*x = (lon - left_lon)/dpp_x;
-	*y = m_height - (lat - bottom_lat)/dpp_y;
+   //LogMessage("NOT IMPLEMENTED: GeoToSurface");
+   double dpp_y, dpp_x;
+   double bottom_lat, left_lon, top_lat, right_lon;
+   GetDegreesPerPixel(&dpp_y, &dpp_x);
+   GetMapBounds(&bottom_lat, &left_lon, &top_lat, &right_lon);
+   *x = (lon - left_lon)/dpp_x;
+   *y = m_height - (lat - bottom_lat)/dpp_y;
 
-//	std::stringstream ss;
-//	ss << "right_lon: " << right_lon;
-//	ss << " lon: " << lon;
-//	ss << " dpp_x: " << dpp_x;
-//	LogMessage(ss.str());
+   //	std::stringstream ss;
+   //	ss << "right_lon: " << right_lon;
+   //	ss << " lon: " << lon;
+   //	ss << " dpp_x: " << dpp_x;
+   //	LogMessage(ss.str());
 }
 
 void AndroidShim::SurfaceToGeo(int x, int y, double* lat, double* lon)
 {
-	LogMessage("NOT IMPLEMENTED: SurfaceToGeo");
+   LogMessage("NOT IMPLEMENTED: SurfaceToGeo");
 }
 
 void AndroidShim::CreatePen(
       const kmlbase::Color32& color, float width, kmldrawing::Pen** pen)
 {
-	//LogMessage("NOT IMPLEMENTED: CreatePen");
-	*pen = new AndroidPen;
-	m_pens_to_delete_after_each_draw.push(*pen);
+   //LogMessage("NOT IMPLEMENTED: CreatePen");
+   *pen = new AndroidPen;
+   m_pens_to_delete_after_each_draw.push(*pen);
 }
 
 void AndroidShim::CreateBrush(
       const kmlbase::Color32& color, kmldrawing::Brush** brush)
 {
-	//LogMessage("NOT IMPLEMENTED: CreateBrush");
-	*brush = new AndroidBrush;
-	m_brushes_to_delete_after_each_draw.push(*brush);
+   //LogMessage("NOT IMPLEMENTED: CreateBrush");
+   *brush = new AndroidBrush;
+   m_brushes_to_delete_after_each_draw.push(*brush);
 }
 
 void AndroidShim::CreateImageFromRawBytes(
       unsigned char* bytes, size_t length, kmldrawing::Image** image,
       bool will_be_used_for_geo /*= false*/)
 {
-	LogMessage("NOT IMPLEMENTED: CreateImageFromRawBytes");
+   LogMessage("NOT IMPLEMENTED: CreateImageFromRawBytes");
 }
 
 void AndroidShim::DrawImage(const kmldrawing::Image& image,
       float left, float top, float right, float bottom, float rotation)
 {
-	LogMessage("NOT IMPLEMENTED: DrawImage");
+   LogMessage("NOT IMPLEMENTED: DrawImage");
 }
 
 void AndroidShim::DrawImageGeo(const kmldrawing::Image& image,
-   double west, double north, double east, double south)
+      double west, double north, double east, double south)
 {
-	LogMessage("NOT IMPLEMENTED: DrawImageGeo");
+   LogMessage("NOT IMPLEMENTED: DrawImageGeo");
 }
 
 void AndroidShim::DrawEllipse(float x, float y, float x_axis, float y_axis,
       const kmldrawing::Pen& pen, const kmldrawing::Brush& brush)
 {
-	//LogMessage("NOT IMPLEMENTED: DrawEllipse");
+   //LogMessage("NOT IMPLEMENTED: DrawEllipse");
 
-	jclass cls = m_env->GetObjectClass(m_obj);
-	jmethodID mid = m_env->GetMethodID(cls, "addEllipse", "(FF)V");
+   jclass cls = m_env->GetObjectClass(m_obj);
+   jmethodID mid = m_env->GetMethodID(cls, "addEllipse", "(FF)V");
 
-	if (mid == 0)
-	{
-		LogMessage("addEllipse not found");
-		return;
-	}
+   if (mid == 0)
+   {
+      LogMessage("addEllipse not found");
+      return;
+   }
 
-	m_env->CallVoidMethod(m_obj, mid, x, y);
+   m_env->CallVoidMethod(m_obj, mid, x, y);
 }
 
 void AndroidShim::DrawPolyPolygon(
@@ -215,59 +224,59 @@ void AndroidShim::DrawPolyPolygon(
       const std::vector<int>& point_counts,
       const kmldrawing::Pen& pen, const kmldrawing::Brush& brush)
 {
-	//LogMessage("NOT IMPLEMENTED: DrawPolyPolygon");
+   //LogMessage("NOT IMPLEMENTED: DrawPolyPolygon");
 
-	jclass cls = m_env->GetObjectClass(m_obj);
-	jmethodID mid = m_env->GetMethodID(cls, "addLine", "(FFFF)V");
+   jclass cls = m_env->GetObjectClass(m_obj);
+   jmethodID mid = m_env->GetMethodID(cls, "addLine", "(FFFF)V");
 
-	if (mid == 0)
-	{
-		LogMessage("addLine not found");
-		return;
-	}
+   if (mid == 0)
+   {
+      LogMessage("addLine not found");
+      return;
+   }
 
-	// TODO: inner rings
+   // TODO: inner rings
 
-	size_t point_count = point_counts[0];
-	for (size_t i = 0; i < point_count - 1; i++)
-		m_env->CallVoidMethod(
-				m_obj, mid, points[i].first, points[i].second, points[i + 1].first, points[i + 1].second);
-	// close the polygon
-	m_env->CallVoidMethod(m_obj, mid, points[point_count - 1].first, points[point_count - 1].second,
-			points[0].first, points[0].second);
+   size_t point_count = point_counts[0];
+   for (size_t i = 0; i < point_count - 1; i++)
+      m_env->CallVoidMethod(
+            m_obj, mid, points[i].first, points[i].second, points[i + 1].first, points[i + 1].second);
+   // close the polygon
+   m_env->CallVoidMethod(m_obj, mid, points[point_count - 1].first, points[point_count - 1].second,
+         points[0].first, points[0].second);
 }
 
 void AndroidShim::DrawLines(const std::vector<std::pair<float, float>>& points,
       const kmldrawing::Pen& pen)
 {
-	LogMessage("NOT IMPLEMENTED: DrawLines");
+   LogMessage("NOT IMPLEMENTED: DrawLines");
 }
 
 void AndroidShim::DrawString(const std::string& text, float x, float y,
-   float size, const kmldrawing::Pen& pen)
+      float size, const kmldrawing::Pen& pen)
 {
-	LogMessage("NOT IMPLEMENTED: DrawString");
+   LogMessage("NOT IMPLEMENTED: DrawString");
 }
 
 void AndroidShim::ShowInformation(
       const std::string& title, const std::string& text)
 {
-	LogMessage("NOT IMPLEMENTED: ShowInformation");
+   LogMessage("NOT IMPLEMENTED: ShowInformation");
 }
 
 void AndroidShim::SetTimer(int id, unsigned long ms)
 {
-	LogMessage("NOT IMPLEMENTED: SetTimer");
+   LogMessage("NOT IMPLEMENTED: SetTimer");
 }
 
 void AndroidShim::ClearAllTimers()
 {
-	//LogMessage("NOT IMPLEMENTED: ClearAllTimers");
+   //LogMessage("NOT IMPLEMENTED: ClearAllTimers");
 }
 
 void AndroidShim::Invalidate(void* ptr)
 {
-	LogMessage("NOT IMPLEMENTED: Invalidate");
+   LogMessage("NOT IMPLEMENTED: Invalidate");
 }
 
 extern std::string TEST_COORDINATES_KML_DATA;
@@ -275,9 +284,36 @@ extern std::string US_STATES_KML;
 
 bool AndroidShim::Fetch(const std::string& uri, std::string* data)
 {
-   //*data = "<kml><Placemark><Point><coordinates>-84,34</coordinates></Point></Placemark></kml>";
-   *data = US_STATES_KML;
-   return true;
+   LogMessage("Entering Fetch");
+
+   jclass cls = m_env->GetObjectClass(m_obj);
+   LogMessage("Calling GetMethodID");
+   jmethodID mid = m_env->GetMethodID(cls, "fetchURL", "(Ljava/lang/String;)Ljava/lang/String;");
+   if (mid == 0)
+   {
+      LogMessage("fetchURL not found");
+      return false;
+   }
+
+   LogMessage("Calling NewStringUTF");
+   jobject jvo = m_env->NewStringUTF(uri.c_str()); // TODO: does this leak???
+   LogMessage("Calling CallObjectMethod");
+   jstring fetched_jstring = (jstring)m_env->CallObjectMethod(m_obj, mid, jvo);
+
+   jboolean is_copy;
+   LogMessage("Calling GetStringUTFChars");
+   const char* fetched_cstring = m_env->GetStringUTFChars(fetched_jstring, &is_copy);
+   *data = fetched_cstring;
+   LogMessage("Calling ReleaseStringUTFChars");
+   m_env->ReleaseStringUTFChars(fetched_jstring, fetched_cstring);
+
+   std::stringstream ss;
+   ss << "Returning from Fetch " << uri << " " << data->length() << " characters";
+   //ss << "  Starts: " << data->substr(0, 40);
+   LogMessage(ss.str().c_str());
+
+   //*data = US_STATES_KML;
+   return data->length() > 0;
 }
 
 bool AndroidShim::ShouldRender(const kmldom::TimePrimitivePtr& time_primitive)
@@ -287,7 +323,7 @@ bool AndroidShim::ShouldRender(const kmldom::TimePrimitivePtr& time_primitive)
 
 void AndroidShim::LogMessage(const std::string& message)
 {
-	__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, message.c_str());
+   __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, message.c_str());
 }
 
 void AndroidShim::CleanupAfterDraw()
--- trunk/KMLDrawer/src/org/falconview/kmldrawer/MainActivity.java
+++ trunk/KMLDrawer/src/org/falconview/kmldrawer/MainActivity.java
@@ -1,5 +1,10 @@
 package org.falconview.kmldrawer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,152 +14,178 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.view.WindowManager;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 	private native void nativeLog(String logThis);
+
 	private native void initDrawer(String kml_url);
+
 	private native void drawKML(int height, int width);
 
-    static {
-        System.loadLibrary("KMLShim");  
-    }
-    
+	static {
+		System.loadLibrary("KMLShim");
+	}
+
 	public class DrawView extends View implements OnTouchListener {
-	    private static final String TAG = "DrawView";
+		//private static final String TAG = "DrawView";
 
-	    List<Point> points = new ArrayList<Point>();
-	    List<Line> lines = new ArrayList<Line>();
-	    Paint paint = new Paint();
+		List<Point> points = new ArrayList<Point>();
+		List<Line> lines = new ArrayList<Line>();
+		Paint paint = new Paint();
 
-	    public DrawView(Context context) {
-	        super(context);
-	        setFocusable(true);
-	        setFocusableInTouchMode(true);
+		public DrawView(Context context) {
+			super(context);
+			setFocusable(true);
+			setFocusableInTouchMode(true);
 
-	        //this.setOnTouchListener(this);
+			// this.setOnTouchListener(this);
 
-	        paint.setColor(Color.WHITE);
-	        paint.setAntiAlias(true);
-	    }
+			paint.setColor(Color.WHITE);
+			paint.setAntiAlias(true);
+		}
 
-	    @Override
-	    public void onDraw(Canvas canvas) {
-	        // draw background
-	    	canvas.drawARGB(255, 0, 0, 0);
-	    	
-	    	// clear objects
-	    	points.clear();
-	    	lines.clear();
-	    	
-	        // draw KML (adds KML objects to draw)
-	        
-	        int height = canvas.getHeight();
-	        int width = canvas.getWidth();
-	        
-	        drawKML(height, width);
-	        
-	        // draw lines
-	        for (Line line : lines) {
-	        	canvas.drawLine(line.a.x, line.a.y, line.b.x, line.b.y, paint);
-	        }
-	        
-	    	// draw points
-	        for (Point point : points) {
-	            canvas.drawCircle(point.x, point.y, 5, paint);
-	            // Log.d(TAG, "Painting: "+point);
-	        }
-	    }
+		@Override
+		public void onDraw(Canvas canvas) {
+			// draw background
+			canvas.drawARGB(255, 0, 0, 0);
 
-	    public boolean onTouch(View view, MotionEvent event) {
-	        // if(event.getAction() != MotionEvent.ACTION_DOWN)
-	        // return super.onTouchEvent(event);
-	        //Log.d(TAG, "point: " + point);
-	    	addEllipse((int)event.getX(), (int)event.getY());
-	        //nativeLog("new touch point: " + point);
-	        invalidate();
-	        return true;
-	    }
-	    
-	    public void addEllipse(float x, float y) {
-	        Point point = new Point();
-	        point.x = x;
-	        point.y = y;
-	        points.add(point);
-	        //nativeLog("new KML point: " + point);
-	    }
-	    
-	    public void addLine(float x1, float y1, float x2, float y2) {
-	    	Line line = new Line();
-	    	
-	        Point point1 = new Point();
-	        point1.x = x1;
-	        point1.y = y1;
-	        line.a = point1;
-	        
-	        Point point2 = new Point();
-	        point2.x = x2;
-	        point2.y = y2;
-	        line.b = point2;
-	        
-	        lines.add(line);
-	        
-	        if (x1 < 1 || y1 < 1 || x2 < 1 || y2 < 1)
-	        	nativeLog("new KML line: " + line);
-	    }
+			// clear objects
+			points.clear();
+			lines.clear();
+
+			// draw KML (adds KML objects to draw)
+
+			int height = canvas.getHeight();
+			int width = canvas.getWidth();
+
+			drawKML(height, width);
+
+			// draw lines
+			for (Line line : lines) {
+				canvas.drawLine(line.a.x, line.a.y, line.b.x, line.b.y, paint);
+			}
+
+			// draw points
+			for (Point point : points) {
+				canvas.drawCircle(point.x, point.y, 5, paint);
+				// Log.d(TAG, "Painting: "+point);
+			}
+		}
+
+		public boolean onTouch(View view, MotionEvent event) {
+			// if(event.getAction() != MotionEvent.ACTION_DOWN)
+			// return super.onTouchEvent(event);
+			// Log.d(TAG, "point: " + point);
+			addEllipse((int) event.getX(), (int) event.getY());
+			// nativeLog("new touch point: " + point);
+			invalidate();
+			return true;
+		}
+
+		public void addEllipse(float x, float y) {
+			Point point = new Point();
+			point.x = x;
+			point.y = y;
+			points.add(point);
+		}
+
+		public void addLine(float x1, float y1, float x2, float y2) {
+			Line line = new Line();
+
+			Point point1 = new Point();
+			point1.x = x1;
+			point1.y = y1;
+			line.a = point1;
+
+			Point point2 = new Point();
+			point2.x = x2;
+			point2.y = y2;
+			line.b = point2;
+
+			lines.add(line);
+		}
 	}
 
 	class Point {
-	    float x, y;
+		float x, y;
 
-	    @Override
-	    public String toString() {
-	        return x + ", " + y;
-	    }
+		@Override
+		public String toString() {
+			return x + ", " + y;
+		}
 	}
 
 	class Line {
-	    Point a, b;
+		Point a, b;
 
-	    @Override
-	    public String toString() {
-	        return a.x + ", " + a.y + " - " + b.x + ", " + b.y;
-	    }
+		@Override
+		public String toString() {
+			return a.x + ", " + a.y + " - " + b.x + ", " + b.y;
+		}
+	}
+
+	DrawView drawView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// allow networking on main thread for now (Ouch!)
+		// for better way, see http://stackoverflow.com/questions/6343166/android-os-networkonmainthreadexception
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
+		// initialize the drawer
+		initDrawer("http://osm-kml.appspot.com/static/us_states.kml");
+
+		// Set full screen view
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		drawView = new DrawView(this);
+		setContentView(drawView);
+		drawView.requestFocus();
+	}
+
+	public void addEllipse(float x, float y) {
+		drawView.addEllipse(x, y);
+	}
+
+	public void addLine(float x1, float y1, float x2, float y2) {
+		drawView.addLine(x1, y1, x2, y2);
 	}
 	
-    DrawView drawView;
+	public String fetchURL(String url_string) {
+		// CALLING nativeLog here causes crashes down the line
+		
+		String inputLine, output = "";
+		StringBuilder sbuilder = new StringBuilder();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // initialize the drawer
-        initDrawer("http://www.example.com/");
-        
-        // Set full screen view
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                                         WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		try {
+			URL url = new URL(url_string);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			while ((inputLine = in.readLine()) != null) {
+				sbuilder.append(inputLine);
+			}
+			in.close();
+			output = sbuilder.toString();
+		} catch (MalformedURLException ex) {
+			output = "MalformedURLException";
+		} catch (IOException ex) {
+			output = "IOException";
+		} catch (Exception ex) {
+			output = "Exception: " + ex.toString();
+		}
 
-        drawView = new DrawView(this);
-        setContentView(drawView);
-        drawView.requestFocus();
-    }
-    
-    public void addEllipse(float x, float y) {
-    	drawView.addEllipse(x, y);
-    }
-    
-    public void addLine(float x1, float y1, float x2, float y2) {
-    	drawView.addLine(x1, y1, x2, y2);
-    }
+		return output;
+	}
 }
